1bf9fa7e690f9a2b9fa223f2d5c909aec3cfafad
TM <tanmayamadhekar15@gmail.com>
2024-07-13 09:39:28 +0530

    Added

--- src/test/java/com/automationtestingrestassured/IDriveLogin.java
+++ src/test/java/com/automationtestingrestassured/IDriveLogin.java
@@ -7,36 +7,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.AssertJUnit.assertEquals;
 
-public class vwoLogin {
+public class IDriveLogin {
 
     @Test
     public void loginWithValid() throws InterruptedException {
         ChromeOptions options=new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         WebDriver driver=new ChromeDriver();
-        driver.get("https://app.vwo.com/#/login");
+        driver.get("https://www.idrive360.com/enterprise/login");
 
         driver.manage().window().maximize();
 
-        WebElement email=driver.findElement(By.id("login-username"));
-        email.sendKeys("vwo@1secmail.com");
+        WebElement email=driver.findElement(By.id("username"));
+        email.sendKeys("augtest_040823@idrive.com");
 
-        WebElement pass=driver.findElement(By.id("login-password"));
-        pass.sendKeys("Vwo@1234");
+        WebElement pass=driver.findElement(By.id("password"));
+        pass.sendKeys("123456");
+        Thread.sleep(3000);
 
-        WebElement rememberME=driver.findElement(By.id("checkbox-remember"));
-        rememberME.isSelected();
-
-        WebElement click_button=driver.findElement(By.id("js-login-btn"));
+        WebElement click_button=driver.findElement(By.id("frm-btn"));
         click_button.click();
 
-        assertThat("Hi V W,", Matchers.equalTo("Hi V W,"));
+        Thread.sleep(5000);
+
+        WebElement message=driver.findElement(By.xpath("//*[@id=\'upgrade]/span"));
+        assertEquals(message.getText(), "Your free trial has expired");
+        assertEquals(driver.getCurrentUrl(),"https://www.idrive360.com/enterprise/account?upgradenow=true");
+
 
         Thread.sleep(8000);
 
